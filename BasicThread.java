4529cff3db07c97dff544bc92e96105af71f928a
M Yaswanth kumar <yashwanthkumarm123@gmail.com>
2024-05-03 01:39:10 +0530

    getpriority(), getname(), start(), run()

--- BasicThread.java
+++ BasicThread.java
@@ -2,25 +2,34 @@ public class BasicThread
 {
     public static void main(String[] args)
     {
+        System.out.println("Thread using runnable interface");
+
         // Create and start two threads
         Thread thread1 = new Thread(new MyRunnable("Thread 1"));
         Thread thread2 = new Thread(new MyRunnable("Thread 2"));
         thread1.start();
         thread2.start();
+        thread1.setPriority(10);
+        System.out.println(thread1.getPriority());
     }
 
     // Runnable implementation defining the task to be performed by the threads
     static class MyRunnable implements Runnable
     {
+
         private final String name;
 
         public MyRunnable(String name)
         {
             this.name = name;
+
         }
         @Override
         public void run()
         {
+            // System.out.println("Thread using runnable interface");
+            // Print 5 times the name of the thread and the number of the iteration
+
             for (int i = 0; i < 5; i++)
             {
                 System.out.println(name + ":" + i);
