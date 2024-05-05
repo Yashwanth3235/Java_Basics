f9014c84d5571a335500470850f5e0b52dd92839
M Yaswanth kumar <yashwanthkumarm123@gmail.com>
2024-05-06 00:50:58 +0530

    Example code using Iterator package

---
+++ IteratorExample.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class IteratorExample {
+    
+    public static void main(String[] args) {
+        // Create an ArrayList
+        ArrayList <String> bikes = new ArrayList<>();
+
+        // Add some new bikes
+        bikes.add("Honda");
+        bikes.add("TVS");
+        bikes.add("Hero");
+        bikes.add("Bajaj");
+
+
+        // Sorting the bikes
+        Collections.sort(bikes);
+
+        // Get an Iterator for the ArrayList
+        Iterator <String> iterator = bikes.iterator();
+
+        // Iterate through the ArrayList using the iterator
+        while (iterator.hasNext())
+        {
+            String bikecompany = iterator.next();
+            System.out.println(bikecompany);
+        }
+    }
+}
+
