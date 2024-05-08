577ad57d9a6f6f96a6918f3c45e650a95f27c3ef
M Yaswanth kumar <yashwanthkumarm123@gmail.com>
2024-05-09 02:00:58 +0530

    basic Example

--- hello.java
+++ hello.java
@@ -1,6 +1,17 @@
+
 class hello
 {
     public static void main(String[] args){
         System.out.println("Hello World");
+        innermethod();
+        
+    }
+
+    static void innermethod()
+    {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
     }
 }
