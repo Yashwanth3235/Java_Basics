49a610120a93c6299c61c5d0bf9e50c137753cd8
M Yaswanth kumar <yashwanthkumarm123@gmail.com>
2024-05-09 19:30:18 +0530

    HashMap basic example code

---
+++ Hashmapexample.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hashmapexample {
+    public static void main(String[] args) {
+        HashMap <Integer, String> Map = new HashMap<Integer, String>();
+        Map.put(1, "Mango"); //put elements in map
+        Map.put(2, "Banana");
+        Map.put(3, "Orange");
+        Map.put(1, "Strawberry");   //trying duplicate iteration (key: 1)
+
+        System.out.println("Iteration HashMap");
+        for (Map.Entry m : Map.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+
+    }
+}
