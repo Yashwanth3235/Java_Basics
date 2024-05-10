3400bbf4fd56081a1b9a8d49fa0e35243108f114
M Yaswanth kumar <yashwanthkumarm123@gmail.com>
2024-05-11 01:21:01 +0530

    using static, abstract and default method example

--- DefaultMethod.java
+++ DefaultMethod.java
@@ -5,16 +5,21 @@ interface Sayable{
 }
 // Abstract method
 void sayMore(String msg);
-}
 
+// Static
+static void Saylouder(String msg){
+    System.out.println(msg);
+}
+}
 public class DefaultMethod implements Sayable{
 
-    public void sayMore(String msg){
+    public void sayMore(String msg){   //implementing abstract method
         System.out.println(msg);
     }
     public static void main(String[] args) {
         DefaultMethod dm = new DefaultMethod();
         dm.say(); //call default method
-        dm.sayMore("Work is worship");  //Call Abstract method
+        dm.sayMore("Work is worship"); //Call Abstract method
+        Sayable.Saylouder("hello world"); 
 }
 }
