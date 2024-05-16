9f33434f04109f336edfc65c642e8be03cc220d8
M Yaswanth kumar <yashwanthkumarm123@gmail.com>
2024-05-17 00:30:13 +0530

    changes

---
+++ mysqldemo.java
@@ -0,0 +1,61 @@
+// import java.sql.Connection;
+// import java.sql.DriverManager;
+// import java.sql.ResultSet;
+// import java.sql.Statement;
+
+// public class mysqldemo {
+//     public static void main(String[] args) {
+//         try{
+//             Class.forName("com.mysql.jdbc.Driver");
+//             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/friends","root","yashuyashu");
+//             Statement stmt = con.createStatement();
+//             ResultSet rs=stmt.executeQuery("select * from student");
+//             while(rs.next())
+//                 System.out.println(rs.getInt(1)+' ' + rs.getString(2) + ' ' + rs.getString(3));
+//                 con.close();
+//                 }
+//                 catch(Exception e){
+//                     System.out.println(e);
+//                 }
+
+//         }
+//     }
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class mysqldemo {
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/friends";
+        String username = "root";
+        String password = "yashuyashu";
+
+        // Register the JDBC driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC driver not found.");
+            e.printStackTrace();
+            return;
+        }
+
+        // Establish the connection
+        try (Connection con = DriverManager.getConnection(jdbcUrl, username, password);
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM student")) {
+
+            // Process the ResultSet
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Database connection error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
+
+
